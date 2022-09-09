012df17352cd22c390fa768bec94709811f21488
Christian Anker Larsen <christian.anker.larsen@maersk.com>
2022-09-09 16:13:47 +0200

    rm unused variable

--- Script.js
+++ Script.js
@@ -1,12 +1,11 @@
 let intervalID;
 function startCountDown(buttonText = document.getElementById("button").innerHTML) {
-    let x;
     if (buttonText === 'START') {
         let totalSeconds = document.getElementById("seconds").innerHTML;
         document.getElementById("button").innerHTML = "STOP";
         intervalID = setInterval(() => {
             if (totalSeconds <= 0) {
-                clearInterval(x);
+                clearInterval(intervalID);
             }
             else{
                 totalSeconds--;
