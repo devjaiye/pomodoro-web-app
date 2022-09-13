1e470da2823af1a001b0414b874d6cd2f44ed13c
Christian Anker Larsen <christian.anker.larsen@maersk.com>
2022-09-13 15:36:09 +0200

    Refactoring and added colour change function

--- Script.js
+++ Script.js
@@ -2,46 +2,84 @@ let intervalID;
 let totalTime = document.getElementById("timer").innerHTML.substring(0, 2) * 60;
 var minutes;
 var seconds;
-function startStopCountDown(buttonText = document.getElementById("button").innerHTML) {
+var breakMode;
+
+function initialiseApp(buttonText = document.getElementById("button").innerHTML) {
     if (buttonText === 'START') {
-        playWorkSound(totalTime);
+        if (totalTime === 1500){
+            playWorkSound();
+        }
         document.getElementById("button").innerHTML = "STOP";
-        countDown();
+        breakMode = false;
+        countDown(breakMode);
     }
     if (buttonText === "STOP"){
         clearInterval(intervalID);
         document.getElementById("button").innerHTML = "START";
     }
 }
-function playWorkSound(timeLeft){
-    if(timeLeft === 1500){
+function playWorkSound(){
         var work = new Audio('resources/work.mp3');
         work.play();
+}
+
+function startBreak(){
+    breakMode = true;
+    document.getElementById("timer").innerHTML = "05:00";
+    totalTime = 300;
+    countDown(breakMode);
+}
+
+function countDown(onABreak){
+    changeBgColor();
+    if (onABreak){
+        intervalID = setInterval(() => {
+            if (totalTime <= 0) {
+                clearInterval(intervalID);
+                //document.getElementById("button").innerHTML = "START";
+                startWork();
+            }
+            decrementTime();
+        }, 1000);
+    }
+    else {
+        intervalID = setInterval(() => {
+            if (totalTime <= 0) {
+                clearInterval(intervalID);
+                //document.getElementById("button").innerHTML = "START";
+                startBreak();
+            }
+            decrementTime();
+        }, 1000);
     }
 }
 
-function startBreakCountDown(){
-    document.getElementById("timer").innerHTML = "05:00";
-    totalTime = 300;
-    countDown();
+function startWork(){
+    breakMode = false;
+    playWorkSound();
+    document.getElementById("timer").innerHTML = "25:00";
+    totalTime = document.getElementById("timer").innerHTML.substring(0, 2) * 60;
+    countDown(breakMode);
 }
 
-function countDown(){
-    intervalID = setInterval(() => {
-        if (totalTime <= 0) {
-            clearInterval(intervalID);
-            //document.getElementById("button").innerHTML = "START";
-            startBreakCountDown();
-        }
-        totalTime--;
-        minutes = Math.floor((totalTime % (1000 * 60 * 60)) / 60);
-        if (minutes < 10){
-            minutes = "0" + minutes;
-        }
-        seconds = totalTime % 60;
-        if (seconds < 10){
-            seconds = "0" + seconds;
-        }
-        document.getElementById("timer").innerHTML = minutes + ":" + seconds;
-    }, 1000);
+function decrementTime(){
+    totalTime--;
+    minutes = Math.floor((totalTime % (1000 * 60 * 60)) / 60);
+    if (minutes < 10){
+        minutes = "0" + minutes;
+    }
+    seconds = totalTime % 60;
+    if (seconds < 10){
+        seconds = "0" + seconds;
+    }
+    document.getElementById("timer").innerHTML = minutes + ":" + seconds;
+}
+
+function changeBgColor(){
+    if (breakMode){
+        document.getElementById("timer").style.backgroundColor = "green"
+    }
+    else {
+        document.getElementById("timer").style.backgroundColor = "red"
+    }
 }
